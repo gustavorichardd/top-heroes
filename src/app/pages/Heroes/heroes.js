22e58f190195a3b21c21666c093a358e25d10b30
Gustavo Richard <gustavo@cityfoods.com.br>
2024-03-21 17:02:22 -0300

    .

--- src/app/pages/Heroes/heroes.js
+++ src/app/pages/Heroes/heroes.js
@@ -12,7 +12,7 @@ import { heroList } from "@/assets/heroesList"
 //Styles
 import styles from './Heroes.module.css'
 
-export default function Heroes() {
+function Heroes() {
     const [alphabeticallOrder, setAlphabeticallOrder] = React.useState("asc")
 
     const getHeroBorder = (rarity) => {
@@ -95,3 +95,5 @@ export default function Heroes() {
         </section>
     )
 }
+
+export default Heroes
