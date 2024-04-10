f9b9561427784a381d0f86b2bd42d547652d64d8
Gustavo Richard <gustavo@cityfoods.com.br>
2024-04-10 12:07:52 -0300

    .

--- src/app/pages/About/about.js
+++ src/app/pages/About/about.js
@@ -3,7 +3,7 @@ import React from "react"
 const About = () => {
 
    return <div>
-      <p>Sobre nós</p>
+      <p>About us</p>
       <p>Created by Kakashi_51 SV29</p>
       <p>Developed by SASUKE SV29</p>
 
